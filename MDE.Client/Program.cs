0b59388f626bb0a31ffc2057f0255acfdf64bcc1
Khaoula Benchari <khaoulabenchari9@gmail.com>
2022-12-18 23:48:30 +0100

    add auth

--- MDE.Client/Program.cs
+++ MDE.Client/Program.cs
@@ -5,7 +5,11 @@ using MDE.Client;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
+builder.Services
+             .AddScoped<IAuthenticationService, AuthenticationService>()
+             .AddScoped<IUserService, UserService>()
+             .AddScoped<IHttpService, HttpService>()
+             .AddScoped<ILocalStorageService, LocalStorageService>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
---
+++ MDE.Client/Services/UserService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using MDE.Client.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MDE.Client.Services
+{
+    public interface IUserService
+    {
+        Task<IEnumerable<User>> GetAll();
+    }
+
+    public class UserService : IUserService
+    {
+        private IHttpService _httpService;
+
+        public UserService(IHttpService httpService)
+        {
+            _httpService = httpService;
+        }
+
+        public async Task<IEnumerable<User>> GetAll()
+        {
+            return await _httpService.Get<IEnumerable<User>>("/users");
+        }
+    }
+}
+
